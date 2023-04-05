429244dde281efefdb73c66aca8bd6db16d9a0ca
robinozer <robin.ozer@outlook.com>
2023-04-05 17:22:08 +0000

    add admin view with list functionalities and ability to confirm bookings

--- bookingapp/admin.py
+++ bookingapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'date_time', 'guests', 'accepted')
+    search_fields = ['first_name', 'email']
+    list_filter = ('accepted', 'date_time')
+    actions = ['accept_booking']
+
+    def accept_booking(self, request, queryset):
+        queryset.update(accepted=True)
+
+"""
+
+from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('date_time', 'guests', 'accepted', 'first_name')
+    search_fields = ['first_name']
+    list_filter = ('accepted', 'date_time')
+
+
+"""
