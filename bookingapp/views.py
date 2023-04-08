4cbb8388699273995083c6a5d6d8379ee4d5cd85
robinozer <robin.ozer@outlook.com>
2023-04-08 17:54:56 +0000

    add view for editing booking and add template and url for the view

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -34,5 +34,25 @@ class BookingCreateView(LoginRequiredMixin, CreateView):
         return response
 
 
+# User redirect page after submitting booking
 def thank_you_view(request):
     return render(request, 'thank_you_view.html')
+
+
+def edit_booking(request):
+    return render(request, 'booking_edit.html')
+
+
+# Update an existing booking
+class BookingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Booking
+    fields = ['first_name', 'email', 'date_time', 'guests', 'special_request']
+    success_url = reverse_lazy('thank_you_view/')
+    template_name = 'booking_edit.html'
+    booking = Booking.objects.get(id=id)
+
+    # Set the current user as the user for the new booking
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        response = super().form_valid(form)
+        return response
