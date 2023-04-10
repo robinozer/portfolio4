5d236feb616d6948e933cdcf4f9016d7280fdc36
robinozer <robin.ozer@outlook.com>
2023-04-10 11:19:15 +0000

    add function to prevent double bookings, however only works when using same email as the other duplicate booking

--- bookingapp/forms.py
+++ bookingapp/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('first_name', 'email', 'guests', 'special_request',)
+        fields = ('first_name', 'email', 'date_time', 'guests', 'special_request',)
--- bookingapp/models.py
+++ bookingapp/models.py
@@ -10,16 +10,14 @@ class Booking(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    # date_time = models.DateTimeField(auto_now=False)
     date_time = models.DateTimeField(validators=[MinValueValidator(limit_value=timezone.now() + timedelta(minutes=1), message=('Please choose a date and time in the future.'))])
-    # date_time = models.DateTimeField(validators=[
-        # MinValueValidator(limit_value=datetime.now() + timedelta(minutes=1), message=_('Please choose a date and time in the future.'))])
     guests = models.IntegerField(validators=[MinValueValidator(1, message="Please enter a value between 1-8"), MaxValueValidator(8, message="Please enter a value between 1-8")])
     special_request = models.TextField()
     accepted = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-date_time']
+        unique_together = ('date_time', 'email')
 
     # String representation of the booking.
     def __str__(self):
